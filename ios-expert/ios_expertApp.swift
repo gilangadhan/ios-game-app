07c96ec9227852ee83fa7d81c59fa2f6edc110c4
magistraapta <magsitraagis@gmail.com>
2024-01-22 13:58:24 +0800

    Initial Commit

---
+++ ios-expert/ios_expertApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_expertApp.swift
+//  ios-expert
+//
+//  Created by Magistra Apta on 22/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ios_expertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
